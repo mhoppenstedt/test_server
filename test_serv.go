325314cc1c34eda2d717dfb09329b7eac60a4ca6
Mitchell Hoppenstedt <hoppenstedt.mitch@gmail.com>
2018-07-18 11:26:57 -0500

    Deleted test_struct and uncommented test_serv

--- test_serv.go
+++ test_serv.go
@@ -1,20 +1,18 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// )
+import (
+	"fmt"
+	"net/http"
+)
 
-// func indexHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Whoa, Go is neat!")
-// }
-
-// func aboutHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Mitchell Hoppenstedt")
-// }
-
-// func main() {
-// 	http.HandleFunc("/", indexHandler)
-// 	http.HandleFunc("/about/", aboutHandler)
-// 	http.ListenAndServe(":8000", nil)
-// }
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Whoa, Go is neat!")
+}
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Mitchell Hoppenstedt")
+}
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/about/", aboutHandler)
+	http.ListenAndServe(":8000", nil)
+}
